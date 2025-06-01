3b297f68c3ad41a4e76e3b05db0c58b17780bb0c
Mark8948 <mark8948yt@gmail.com>
2025-06-01 19:51:46 +0200

    Creazione tabelle
    Prima creazione di tabelle con bozze senza implementare collegamenti tra loro.

---
+++ src/main/java/it/uniroma3/siw/model/User.java
@@ -0,0 +1,47 @@
+package it.uniroma3.siw.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class User {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	private String name;
+	private String surname;
+	private String CF;  //CODICE FISCALE
+	private int dateOfBirth; //In format MMDDYYYY
+	
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getSurname() {
+		return surname;
+	}
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+	public String getCF() {
+		return CF;
+	}
+	public void setCF(String cF) {
+		CF = cF;
+	}
+	public int getDateOfBirth() {
+		return dateOfBirth;
+	}
+	public void setDateOfBirth(int dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+	}
+	
+	
+}
