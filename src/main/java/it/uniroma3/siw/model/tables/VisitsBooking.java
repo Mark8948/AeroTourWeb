085fce820a6772f75b9794e29ad6e6bd1999282a
Mark8948 <mark8948yt@gmail.com>
2025-06-03 14:25:07 +0200

    General fix and image implementation in html

--- src/main/java/it/uniroma3/siw/model/tables/VisitsBooking.java
+++ src/main/java/it/uniroma3/siw/model/tables/VisitsBooking.java
@@ -16,69 +16,71 @@ import jakarta.persistence.ManyToOne;
 
 @Entity
 public class VisitsBooking {
-	
+
 	@Id
-	@GeneratedValue(strategy=GenerationType.AUTO)
-	private Long Id;
-	
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+
 	private String guideName;
-	
-	@Column(name = "date_time_visit")
+
+	@Column(name = "visit_date_time", nullable = false)
 	private LocalDateTime visitDateTime;
-	
+
 	@Column(length = 10)
 	private Integer guideCellNumber;
-	
+
 	@Enumerated(EnumType.STRING)
+	@Column(nullable = false)
 	private Status status = Status.IN_ATTESA_DI_CONFERMA;
 
 	@ManyToOne
 	private Users user;
-	
-	
+
 	public Users getUser() {
 		return user;
 	}
+
 	public void setUser(Users user) {
 		this.user = user;
 	}
+
 	public Long getId() {
-		return Id;
+		return id;
 	}
+
 	public void setId(Long id) {
-		Id = id;
+		this.id = id;
 	}
+
 	public LocalDateTime getVisitDateTime() {
 		return visitDateTime;
 	}
+
 	public void setVisitDateTime(LocalDateTime visitDateTime) {
 		this.visitDateTime = visitDateTime;
 	}
+
 	public Status getStatus() {
 		return status;
 	}
+
 	public void setStatus(Status status) {
 		this.status = status;
 	}
+
 	public String getGuideName() {
 		return guideName;
 	}
+
 	public void setGuideName(String guideName) {
 		this.guideName = guideName;
 	}
-	public LocalDateTime getVisitDate() {
-		return visitDateTime;
-	}
-	public void setVisitDate(LocalDateTime date) {
-		this.visitDateTime = date;
-	}
+
 	public Integer getGuideCellNumber() {
 		return guideCellNumber;
 	}
+
 	public void setGuideCellNumber(Integer guideCellNumber) {
 		this.guideCellNumber = guideCellNumber;
 	}
-	
-	
-	
 }
---
+++ src/main/java/it/uniroma3/siw/repository/VisitsBookingRepository.java
@@ -0,0 +1,11 @@
+package it.uniroma3.siw.repository;
+
+import org.springframework.data.repository.CrudRepository;
+//import java.util.List;
+import java.util.Optional;
+
+import it.uniroma3.siw.model.tables.VisitsBooking;
+
+public interface VisitsBookingRepository extends CrudRepository<VisitsBooking, Long>{
+	public Optional<VisitsBooking> findById(Long id);
+}
