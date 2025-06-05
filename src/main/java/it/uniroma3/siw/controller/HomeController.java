c1aeca702d6cb0d49ee0dc1d496065995f819f7e
Mark8948 <mark8948yt@gmail.com>
2025-06-05 14:21:41 +0200

    fixed index to be home

--- src/main/java/it/uniroma3/siw/controller/HomeController.java
+++ src/main/java/it/uniroma3/siw/controller/HomeController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 @Controller
 public class HomeController {
 	
-	@GetMapping({"/", "index"})
+	@GetMapping({"/", "home"})
 	public String home(Model model) { //Model model serve a passare i dati alla home con tutti gli aerei.
 			return "homepage";
 	}
