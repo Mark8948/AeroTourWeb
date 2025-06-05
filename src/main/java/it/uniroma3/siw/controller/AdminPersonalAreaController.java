1a2dc70127b3a6e068fc1af496a6f673fe0c5f61
Mark8948 <mark8948yt@gmail.com>
2025-06-05 17:33:20 +0200

    added personal areas and error handling

---
+++ src/main/java/it/uniroma3/siw/controller/AdminPersonalAreaController.java
@@ -0,0 +1,14 @@
+package it.uniroma3.siw.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class AdminPersonalAreaController {
+	
+	@GetMapping("admin")
+	public String home(Model model) { //Model model serve a passare i dati alla home con tutti gli aerei.
+			return "adminPersonalArea";
+	}
+}
